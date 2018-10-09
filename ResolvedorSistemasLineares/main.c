7cafd61adbddf322fd744bbb76883873828184e8
LucaDillenburg <luca.assumpcao.dillenburg@gmail.com>
2018-10-09 11:55:31 -0300

    Versão com ListaLigada completa (ainda não juntado)

--- ResolvedorSistemasLineares/main.c
+++ ResolvedorSistemasLineares/main.c
@@ -1,64 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double* resolverSistema(char* nomeArq, char*** incongnitas, int* ordem);
-
+//matriz -> solucao
+double* resolverSistema(double** matriz, int* ordem);
 char fazerEscalonamento(double** matriz, int ordem);
-
 void zerarColunaDeOutrasLinhas(double** matriz, int indexColLinha, int ordem);
-
 int procurarLinhaSemZeroEmColuna(double** matriz, int linhaInicial, int coluna, int ordem);
 void trocarLinhas(double** matriz, int nLinha1, int nLinha2);
 void multLinha(double** matriz, int indexLinha, double multiplicador, int ordem);
 void multLinhaSomarOutra(double** matriz, int indexLinhaMultiplicada, double mult, int indexLinhaReceber, int ordem);
-
 void printarMatriz(double** matriz, int linhas, int colunas);
+//lista
+typedef struct sNo
+{
+    void* info;
+    struct sNo* prox;
+} No;
+typedef struct
+{
+    No* prim;
+    char (*equals) (void*, void*);
+} Lista;
+void inserirComeco(Lista* lista, void* info);
+void inserirFinal(Lista* lista, void* info);
+void inserirEm(Lista* lista, void* info, int onde);
+void removerComeco(Lista* lista);
+void removerFinal(Lista* lista);
+void removerEm(Lista* lista, int onde);
+int ondeEsta (Lista* lista, void* info);
+void printLista(Lista* lista);
+char equalsStr (char** str1, char** str2);
 
 int main()
 {
-    double** matriz = (double**)malloc(sizeof(double*)*3);
-    int i;
-    for (i=0; i<3; i++)
-        matriz[i] = (double*)malloc(sizeof(double)*4);
+    printf("Escreva o nome do arquivo: ");
+    char* nomeArq = (char*)malloc(sizeof(char)*100);
+    scanf("%s", nomeArq);
 
-    matriz[0][0] = 3;
-    matriz[0][1] = 0;
-    matriz[0][2] = 1;
-    matriz[0][3] = -10;
+    int ordem;
+    Lista* incognitas;
+    double** matriz = matrizFromArquivo(nomeArq, &ordem, incognitas);
 
-    matriz[1][0] = 2;
-    matriz[1][1] = 7;
-    matriz[1][2] = 8;
-    matriz[1][3] = 15;
+    double* solucao = resolverSistema(matriz, ordem);
 
-    matriz[2][0] = 0;
-    matriz[2][1] = 1;
-    matriz[2][2] = 4;
-    matriz[2][3] = 10;
-
-    printarMatriz(matriz, 3, 4);
-    printf("\n\n");
-
-    char res = fazerEscalonamento(matriz, 3);
-    if (!res)
-        return NULL;
-    //colocar valores em int*, char***
-    double* solucao = (int*)malloc(sizeof(int)*3);
-    for(i=0; i<3; i++)
-        *(solucao+i) = *(*(matriz+i)+3);
-
-    for(i=0; i<3; i++)
-        printf("%lf\n", solucao[i]);
+    if (solucao == NULL)
+    {
+        printf("O sistema não pode ser resolvido!");
+        return -1;
+    }
 
     return 0;
 }
 
-double* resolverSistema(char* nomeArq, char*** incongnitas, int* ordem)
+double* resolverSistema(double** matriz, int* ordem)
 {
-    double** matriz;
-    //colocar elementos na matriz e mudar *ordem
-    //colocar incognitas em char***
-
     //fazer escalonamento
     char res = fazerEscalonamento(matriz, *ordem);
     if (!res)
@@ -164,3 +159,151 @@ void printarMatriz(double** matriz, int linhas, int colunas)
     }
 }
 
+
+
+
+    /*LISTA LIGADA*/
+
+/* INSERIR */
+void inserirComeco(Lista* lista, void* info)
+{
+    No* novoPrim = (No*)malloc(sizeof(No));
+    (*novoPrim).info = info;
+    (*novoPrim).prox = (*lista).prim;
+
+    (*lista).prim = novoPrim;
+}
+
+void inserirFinal(Lista* lista, void* info)
+{
+    if ((*lista).prim == NULL)
+    {
+        inserirComeco(lista, info);
+        return;
+    }
+
+    No* atual = (*lista).prim;
+    while ((*atual).prox != NULL)
+        atual = (*atual).prox;
+
+    No* novo = (No*)malloc(sizeof(No));
+    (*novo).info = info;
+    (*novo).prox = NULL;
+
+    (*atual).prox = novo;
+}
+
+void inserirEm(Lista* lista, void* info, int onde)
+{
+    if (onde == 0)
+    {
+        inserirComeco(lista, info);
+        return;
+    }
+
+    No* atual = (*lista).prim;
+    int i;
+    for(i=1; i<onde; i++)
+    {
+        if (atual == NULL)
+            return;
+        atual = (*atual).prox;
+    }
+
+    /*deve ser adicionado o noh no atual.prox*/
+    No* novo= (No*)malloc(sizeof(No));
+    (*novo).info = info;
+    (*novo).prox = (*atual).prox;
+
+    (*atual).prox = novo;
+}
+
+/*REMOVER*/
+void removerComeco(Lista* lista)
+{
+    if ((*lista).prim == NULL)
+        return;
+
+    No* aux = (*lista).prim;
+    (*lista).prim = (*(*lista).prim).prox;
+    free(aux);
+}
+
+void removerFinal(Lista* lista)
+{
+    if ((*lista).prim == NULL)
+        return;
+
+    No* anterior = NULL;
+    No* atual = (*lista).prim;
+    while ((*atual).prox != NULL)
+    {
+        anterior = atual;
+        atual = (*atual).prox;
+    }
+
+    if (anterior == NULL) //soh tinha um elemento
+        (*lista).prim = NULL;
+    else
+        (*anterior).prox = NULL;
+
+    //libera o espaco do ultimo Noh
+    free(atual);
+}
+
+void removerEm(Lista* lista, int onde)
+{
+    if (onde == 0)
+    {
+        removerComeco(lista);
+        return;
+    }
+
+    No* atual = (*lista).prim;
+    int i;
+    for(i=1; i<onde; i++)
+    {
+        if (atual == NULL)
+            return;
+        atual = (*atual).prox;
+    }
+
+    /*atual.prox deve ser removido*/
+    No* aux = (*atual).prox;
+    if (aux == NULL)
+        return;
+    (*atual).prox = (*(*atual).prox).prox;
+
+    //libera o espaco do noh removido
+    free(aux);
+}
+
+/*OUTROS*/
+int ondeEsta (Lista* lista, void* info)
+{
+    No* atual = (*lista).prim;
+    int i = 0;
+    for(; atual != NULL; i++, atual = (*atual).prox)
+        if( (*(*lista).equals)((*atual).info, info) )
+            return i;
+
+    return -1;
+}
+
+
+void printLista(Lista* lista)
+{
+    No* atual = (*lista).prim;
+    while(atual != NULL)
+    {
+        printf("%s -> ", (*atual).info);
+        atual = (*atual).prox;
+    }
+    printf("null");
+}
+
+/**/
+char equalsStr (char** str1, char** str2)
+{
+    return strcmp(*str1, *str2) == 0;
+}
