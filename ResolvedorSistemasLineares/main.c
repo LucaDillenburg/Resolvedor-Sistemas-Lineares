2608d4e679363d9ba21c137d5ca23572d9c05ed0
LucaDillenburg <LucaDillenburg@users.noreply.github.com>
2018-10-11 00:52:47 -0300

    Código limpo e organizado + explicação do método (escalonamento)
    Novo:
     - organizei e limpei o código
     - printei a solução
     - dei a opção do usuário ver a resolução do sistema (com textos de explicação)
    
    O que falta:
     - ERROS:
       .se há números dentro do nome das incógnitas da erro de execução
      .se termo independente está do lado esquerdo dar erro
     - COISAS A MAIS:
        .poder colocar numero decimal (com ponto e não vírgula)

--- ResolvedorSistemasLineares/main.c
+++ ResolvedorSistemasLineares/main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//matriz -> solucao
-double* resolverSistema(double** matriz, int* ordem);
-char fazerEscalonamento(double** matriz, int ordem);
-void zerarColunaDeOutrasLinhas(double** matriz, int indexColLinha, int ordem);
-int procurarLinhaSemZeroEmColuna(double** matriz, int linhaInicial, int coluna, int ordem);
-void trocarLinhas(double** matriz, int nLinha1, int nLinha2);
-void multLinha(double** matriz, int indexLinha, double multiplicador, int ordem);
-void multLinhaSomarOutra(double** matriz, int indexLinhaMultiplicada, double mult, int indexLinhaReceber, int ordem);
-void printarMatriz(double** matriz, int linhas, int colunas);
+#include <string.h>
+#include <math.h>
+
 //lista
 typedef struct sNo
 {
@@ -28,8 +22,25 @@ void removerComeco(Lista* lista);
 void removerFinal(Lista* lista);
 void removerEm(Lista* lista, int onde);
 int ondeEsta (Lista* lista, void* info);
+void* getElemento(Lista* lista, int n);
 void printLista(Lista* lista);
-char equalsStr (char** str1, char** str2);
+char equalsStr (char* str1, char* str2);
+//file -> matriz
+double** fileParaMatriz(char* nomeArquivo, int* ordem, Lista* incognitas);
+char* strFromFile(char* nomeArquivo, int* ordem);
+double** mallocMatriz(int ordem);
+void resetarVariaveis(char** str1, char** str2);
+//matriz -> solucao
+double* resolverSistema(double** matriz, int ordem, Lista* incognitas);
+char fazerEscalonamento(double** matriz, int ordem, Lista* incognitas);
+void zerarColunaDeOutrasLinhas(double** matriz, int indexColLinha, int ordem);
+int procurarLinhaSemZeroEmColuna(double** matriz, int linhaInicial, int coluna, int ordem);
+void trocarLinhas(double** matriz, int nLinha1, int nLinha2);
+void multLinha(double** matriz, int indexLinha, double multiplicador, int ordem);
+void multLinhaSomarOutra(double** matriz, int indexLinhaMultiplicada, double mult, int indexLinhaReceber, int ordem);
+void printarSistema(double** matriz, Lista* incognitas, int ordem);
+
+void printarMatriz(double** matriz, int linhas, int colunas);
 
 int main()
 {
@@ -37,61 +48,298 @@ int main()
     char* nomeArq = (char*)malloc(sizeof(char)*100);
     scanf("%s", nomeArq);
 
-    int ordem;
-    Lista* incognitas;
-    double** matriz = matrizFromArquivo(nomeArq, &ordem, incognitas);
+    printf("\n");
 
-    double* solucao = resolverSistema(matriz, ordem);
+    int ordem;
+    Lista* incognitas = (Lista*)malloc(sizeof(Lista));
+    (*incognitas).prim = NULL;
+    (*incognitas).equals = (char(*)(void*,void*))&equalsStr;
+    double** matriz = fileParaMatriz(nomeArq, &ordem, incognitas);
+
+    if (matriz ==NULL)
+    {
+        printf("O numero de variaveis no arquivo nao corresponde ao numero de variaveis escrito no decorrer do arquivo!");
+        return -2;
+    }
+
+    char resposta = 'n';
+    do
+    {
+        //perguntar se usuario qr ver todos os passos do escalonamento
+        printf("Voce gostaria de ver o passo a passo da resolucao do sistema? S/N\n");
+        scanf(" %c", &resposta);
+    }while(resposta != 'S' && resposta != 'N' && resposta != 's' && resposta != 'n');
+
+    char querVerPassoAPasso = resposta == 'S' || resposta == 's';
+    double* solucao = resolverSistema(matriz, ordem, querVerPassoAPasso?incognitas:NULL);
 
     if (solucao == NULL)
     {
-        printf("O sistema não pode ser resolvido!");
+        printf("O sistema eh SPI (Sistema Possivel Indeterminado) ou SI (Sistema Impossivel)!");
         return -1;
     }
+
+    //printar solucao
+    printf("\nSolucao:\n");
+    int i;
+    for (i = 0; i<ordem; i++)
+        printf("    %s = %lf\n", (char*)getElemento(incognitas, i), *(solucao+i));
 
     return 0;
 }
 
-double* resolverSistema(double** matriz, int* ordem)
+
+    /*FILE PARA MATRIZ*/
+double** fileParaMatriz(char* nomeArquivo, int* ordem, Lista* incognitas)
+{
+    char* strArq = strFromFile(nomeArquivo, ordem);
+    double** matriz = mallocMatriz(*ordem);
+
+    printf("SISTEMA ESCRITO NO ARQUIVO:\n", *ordem);
+    printf("%s\n\n", strArq);
+
+    int auxSinal = 1;
+    char* auxInt;
+    char* auxStr;
+    resetarVariaveis(&auxInt, &auxStr);
+
+    int qtdIncognitas = 0;
+
+    int linha = 0;
+    int i;
+    for(i = 0; ; i++)
+    {
+        double valor;
+        if(*(strArq+i) == '-' || *(strArq+i) == '+' || *(strArq+i) == '=' || *(strArq+i) == '\n' || *(strArq+i) == '\0')
+        {
+            valor = (double)(((*auxInt=='\0')?1:atoi(auxInt))*auxSinal);
+            if(*auxStr != '\0')
+                if(ondeEsta(incognitas, auxStr) < 0)
+                {
+                    //verifica se nao ha incognitas a mais do que deveria
+                    if (qtdIncognitas >= *ordem)
+                        return NULL;
+                    inserirFinal(incognitas, auxStr);
+                    qtdIncognitas++;
+                }
+
+            if(*auxStr == '\0')
+            {
+                if(*(strArq+i) == '\n' || *(strArq+i) == '\0')
+                    *(*(matriz+linha)+*ordem) += valor;
+            }
+            else
+                *(*(matriz+linha)+ondeEsta(incognitas, auxStr)) += valor;
+
+            auxSinal  = 1;
+            resetarVariaveis(&auxInt, &auxStr);
+        }
+
+        if(*(strArq+i) == '-' )
+            auxSinal  = -1;
+
+        if(*(strArq+i) == '\n')
+        {
+            linha++;
+            //verifica se nao ha mais equacoes do que variaveis: considera as [ordem] primeiras apenas
+            if (linha >= *ordem)
+                break;
+        }
+
+        if(*(strArq+i) == '\0')
+            break;
+
+        if(isalpha(*(strArq+i)))
+        {
+            int j;
+            for(j = 0;;j++)
+                if(*(auxStr+j) == '\0')
+                {
+                    *(auxStr+j) = *(strArq+i);
+                    *(auxStr+(j + 1)) = '\0';
+                    break;
+                }
+        }else
+        if(isdigit(*(strArq+i)))
+        {
+            int j;
+            for(j = 0;;j++)
+                if(*(auxInt+j) == '\0')
+                {
+                    *(auxInt+j) = *(strArq+i);
+                    *(auxInt+(j + 1)) = '\0';
+                    break;
+                }
+        }
+    }
+
+    /*Free tudo que deu malloc*/
+    free(auxStr);
+    free(auxInt);
+    free(strArq);
+
+    //verifica se nao ha incognitas a menor do que deveria
+    if (qtdIncognitas != *ordem)
+        return NULL;
+    return matriz;
+}
+
+char* strFromFile(char* nomeArquivo, int* ordem)
+{
+    /*Lendo o arquivo*/
+    FILE* file = fopen(nomeArquivo, "r");
+    char* auxRet = (char*)malloc(sizeof(char) * 7500);
+    char* auxConcat = (char*)malloc(sizeof(char) * 1024);
+    *auxRet = '\0';
+
+    fscanf(file, "%i", ordem);
+    fgets(auxConcat, 1024, file);
+    while(fgets(auxConcat, 1024, file) != NULL)
+        strcat(auxRet, auxConcat);
+
+    //criar char* com strlen(aux) + 1
+    char* ret = (char*)malloc(sizeof(char)*(strlen(auxRet) + 1));
+    strcpy(ret, auxRet);
+
+    /*Fim da leitura do arquivo*/
+    free(auxConcat);
+    free(auxRet);
+    fclose(file);
+
+    return ret;
+}
+
+double** mallocMatriz(int ordem)
+{
+    //instanciacao
+    double** matriz = (double**)malloc(ordem * sizeof(double*));
+    int i;
+    for(i = 0; i < ordem; i++)
+        *(matriz+i) = (double*)malloc((ordem + 1) * sizeof(double));
+
+    int j;
+    //deixar matriz inteira de zeros
+    for(i = 0; i < ordem; i++)
+        for(j = 0; j < (ordem + 1); j++)
+            *(*(matriz+i)+j) = 0;
+
+    return matriz;
+}
+
+void resetarVariaveis(char** str1, char** str2)
+{
+    *str1 = (char*)malloc(sizeof(char) * 255);
+    **str1  = '\0';
+    *str2 = (char*)malloc(sizeof(char) * 255);
+    **str2  = '\0';
+}
+
+
+    /*RESOLVER SISTEMA*/
+double* resolverSistema(double** matriz, int ordem, Lista* incognitas)
 {
     //fazer escalonamento
-    char res = fazerEscalonamento(matriz, *ordem);
+    char res = fazerEscalonamento(matriz, ordem, incognitas);
     if (!res)
         return NULL;
 
     //colocar valores em int*, char***
-    double* solucao = (double*)malloc(sizeof(double)* *ordem);
+    double* solucao = (double*)malloc(sizeof(double)*ordem);
     int i;
-    for(i=0; i<*ordem; i++)
-        *(solucao+i) = *(*(matriz+i)+*ordem);
+    for(i=0; i<ordem; i++)
+        *(solucao+i) = *(*(matriz+i)+ordem);
 
     return solucao;
 }
 
-char fazerEscalonamento(double** matriz, int ordem)
+char fazerEscalonamento(double** matriz, int ordem, Lista* incognitas)
 {
+    if (incognitas != NULL)
+    {
+        printf("\n\n\nSomando todas as variaveis que estavam duplicadas o sistema fica da seguinte forma:\n");
+        printarSistema(matriz, incognitas, ordem);
+
+        printf("\nO metodo que vamos usar eh o do escalonamento. Teremos que buscar deixar a diagonal principal do sistema sendo um e o resto igual a zero.");
+        printf("Para isso poderemos multiplicar as linhas por um certo valor, trocar linhas de posicao ou multiplicar com uma linha e somar com outra.\n\n");
+    }
+
     int i;
+    int passo = 1;
     for (i = 0; i<ordem; i++)
     {
         double elem = *(*(matriz+i)+i);
         if (elem == 0)
         {
+            if (incognitas != NULL)
+            {
+                printf("\n\n\n  -> Passo %i (linha atual: %i):\n", passo, i+1);
+                printf(" - Como o elemento dessa equacao da diagonal principal do sistema eh zero, ");
+                printf("teremos que achar outra linha abaixo dessa que tenha um numero diferente de zero nessa coluna (coluna %i).\n", i+1);
+            }
+
             int nLinha = procurarLinhaSemZeroEmColuna(matriz, i + 1, i, ordem);
+
+            if (incognitas != NULL)
+            {
+                if (nLinha >= 0)
+                {
+                    printf(" - Na linha %i, o elemento da coluna %i eh diferente de zero! ");
+                    printf("Entao vamos trocar essa linha com a linha em que o elemento da diagonal principal era nulo!\n\n", nLinha+1, i+1);
+                }
+                else
+                    printf(" - Nao ha nenhuma outra linha com o elemento da coluna %i diferente de zero!\n\n");
+            }
+
             if (nLinha < 0)
-                return 0; //erro de execucao (nao eh possivel resolver sistema)
+                return 0; //erro: sistema invalido (nao eh possivel resolver sistema)
             trocarLinhas(matriz, i, nLinha);
 
+            if (incognitas != NULL)
+            {
+                printf("Apos trocar as linhas %i e %i do sistema, ele ficou da seguinte forma:\n", i+1, nLinha+1);
+                printarSistema(matriz, incognitas, ordem);
+            }
+
             elem = *(*(matriz+i)+i);
+
+            passo++;
         }
 
-        multLinha(matriz, i, 1/elem, ordem);
-        *(*(matriz+i)+i) = 1; //colocar como sendo 1 (pode ficar aproximadamente isso)
+        double multiplicador = 1/elem;
+        multLinha(matriz, i, multiplicador, ordem);
+        if (incognitas != NULL)
+        {
+            printf("\n\n\n  -> Passo %i (linha atual: %i):\n", passo, i+1);
+            printf(" - Depois de deixar o elemento da diagonal principal da linha atual diferente de zero, ");
+            printf("deve-se multiplicar a linha por um numero para que o elemento torne-se um.\n");
+            printf(" - Nesse caso o multiplicador eh %.2lf pois 1/%.2lf = %.2lf\n\n", multiplicador, elem, multiplicador);
+
+            printf("Apos multiplicar a fileira inteira por %.2lf, o sistema ficou da seguinte maneira:\n");
+            printarSistema(matriz, incognitas, ordem);
+        }
+        passo++;
+
+        //*(*(matriz+i)+i) = 1; //colocar como sendo 1 (pode ficar aproximadamente isso)
 
         zerarColunaDeOutrasLinhas(matriz, i, ordem);
+        if (incognitas != NULL)
+        {
+            printf("\n\n\n  -> Passo %i (linha atual: %i):\n", passo, i+1);
+            printf(" - Entao devemos fazer um procedimento com todas as outras linhas: multiplicar essa equacao por um numero e somar a mesma com a outra.\n");
+            printf(" - Os multiplicadores da linha atual para cada outra linha deve ser escolhido de forma a garantir que a linha que vai ser somada fique ");
+            printf("com zero no elemento da coluna %i.\n\n", i+1);
+
+            printf("Apos fazer esse processo com todas as linhas, o sistema ficou assim:\n");
+            printarSistema(matriz, incognitas, ordem);
+        }
+        passo++;
     }
 
-    printarMatriz(matriz, ordem, ordem+1);
-    printf("\n\n");
+    if (incognitas != NULL)
+    {
+        printf("\n\n\nApos o procedimento feito na ultima linha, o sistema jah estah completamente escalonado. ");
+        printf("\nBasta atribuir o termo independente a variavel restante em cada linha e essa serah a solucao!\n");
+    }
 
     return 1; //execucao normal (deu certo)
 }
@@ -147,6 +395,32 @@ void trocarLinhas(double** matriz, int nLinha1, int nLinha2)
     *(matriz+nLinha2) = aux;
 }
 
+void printarSistema(double** matriz, Lista* incognitas, int ordem)
+{
+    int i, j;
+    for(i = 0; i<ordem; i++)
+    {
+        for (j=0; j<ordem; j++)
+        {
+            if (*(*(matriz+i)+j) >= 0)
+            {
+                if (j!=0)
+                    printf(" + ");
+            }
+            else
+            {
+                if (j!=0)
+                    printf(" - ");
+                else
+                    printf("-");
+            }
+            printf("%.2lf%s", fabs(*(*(matriz+i)+j)), getElemento(incognitas, j));
+        }
+        printf(" = %.2lf\n", *(*(matriz+i)+ordem));
+    }
+}
+
+
 void printarMatriz(double** matriz, int linhas, int colunas)
 {
     int i;
@@ -154,7 +428,7 @@ void printarMatriz(double** matriz, int linhas, int colunas)
     for(i=0; i<linhas; i++)
     {
         for(j=0; j<colunas; j++)
-            printf("%lf  ", matriz[i][j]);
+            printf("%lf  ", *(*(matriz+i)+j));
         printf("\n");
     }
 }
@@ -290,8 +564,17 @@ int ondeEsta (Lista* lista, void* info)
     return -1;
 }
 
+void* getElemento(Lista* lista, int n)
+{
+    No* atual = (*lista).prim;
+    int i;
+    for (i=0; atual != NULL; i++, atual = (*atual).prox)
+        if (i == n)
+            return (*atual).info;
+    return NULL;
+}
 
-void printLista(Lista* lista)
+void printListaStr(Lista* lista)
 {
     No* atual = (*lista).prim;
     while(atual != NULL)
@@ -303,7 +586,7 @@ void printLista(Lista* lista)
 }
 
 /**/
-char equalsStr (char** str1, char** str2)
+char equalsStr (char* str1, char* str2)
 {
-    return strcmp(*str1, *str2) == 0;
+    return strcmp(str1, str2) == 0;
 }
